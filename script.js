16601e39c118a235a25e55748e5467f8981cc815
utku-guclu <stanger307@live.com>
2022-01-21 07:13:09 -0800

    added sfx

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let message = document.querySelector("h1");
 let dices = document.querySelectorAll(".dice");
-
+let audio = new Audio("./sfx/dice.mp3")
 
 function game() {
     let x = [];
@@ -9,9 +9,10 @@ function game() {
         let random_number = Math.floor(Math.floor(Math.random() * 6)) + 1;
         
         x.push(random_number);
-        console.log(x)
+        
         window.addEventListener("load", () => {
             item.firstElementChild.nextElementSibling.setAttribute("src", `./images/dice${random_number}.png`);
+            audio.play();
         })
     })
 
